11c559886fce1dd398d56dd61e287397f9dc587e
Maxim Alferov <malferov44@gmail.com>
2017-09-27 18:04:55 +0200

    mi module added

---
+++ mi.py
@@ -0,0 +1,39 @@
+import sys
+import time
+#import subprocess
+#import requests
+from sign.signdef import *
+
+class MI:
+    def __init__(self):
+        self.wmsigner = "sign/wmsigner"
+        self.host = "credit.webmoney.ru"
+    def ts_exit(self, msg):
+        sys.exit(time.strftime("%Y-%m-%d %H:%M:%S ") + str(msg))
+"""
+    def sign(self, ss):
+        try:
+            return str(subprocess.check_output([self.wmsigner, "-s", ss]), 'ascii')
+        except subprocess.CalledProcessError as e:
+            self.ts_exit(e.output)
+    def getPortfolio(self, tid):
+        path = "/ZTenders.ashx"
+        t = str(int(time.time())) + '000'
+        ss = self.sign(mWMID + ';' + tid + ';' + t)
+        params = "WMID="+mWMID+"&TID="+tid+"&t="+t+"&VR=1&SS="+ss
+        return self.httpReq('https', path, params, 'get')
+#        return [t, str(self.sign(t))]
+    def httpReq(self, proto, path, params, reqtype):
+        session = requests.Session()
+        if reqtype == 'get':
+            r = session.get(proto + '://' + self.host + path + '?' + params)
+        elif reqtype == 'post':
+            payload = params
+            r = session.post(proto + '://' + self.host + path, data = payload)
+        else:
+            self.ts_exit("httpReq: illegal reqtype")
+        if r.status_code != 200:
+            self.ts_exit("httpReq: http_code = " + str(r.status_code))
+        else:
+            return r.text
+"""
---
+++ mintos.py
@@ -0,0 +1,78 @@
+#!/bin/env python
+import sys
+import time
+import json
+#from cwm import CWM
+from mi import MI
+
+H = '/home/malf/mintos/' # home dir
+#NUMTDR = H + 'dat' + OS + 'numtdr.dat' # status file
+#require_once('WMSigner.php');
+#include ('k'.OS.'signdef.php');
+#include('cwm.php');
+#include('loandef.php');
+#include('errdesc.php');
+#include('/home/srv30502/htdocs/trk/sms/smstraffic/sms.php');
+
+class Runner(MI):
+    def __init__(self, h):
+        super().__init__()
+        self.h = h
+        self.varset = {"status": {"file": "dat/numtdr.dat", "var": ""}}
+        self.var_load("status")
+        self.lock()
+    def sync(self, varname):
+        j = json.dumps(self.varset[varname]["var"], sort_keys=True, indent=4)#separators=(',',':')
+        open(self.h + self.varset[varname]["file"], 'w').write(j)
+    def var_load(self, varname):
+        self.varset[varname]["var"] = json.loads(open(self.h + self.varset[varname]["file"]).read())
+    def lock(self):
+        if self.varset["status"]["var"][2] == 1:
+            self.ts_exit("System is locked")
+        else:
+            self.varset["status"]["var"][2] = 1
+            self.sync("status")
+    def ulock(self):
+        self.varset["status"]["var"][2] = 0
+        self.sync("status")
+
+class Mintos(Runner):
+    def __init__(self, h):
+        super().__init__(h)
+#        self.varset["portfolio"] = {"file": "dat/port/port000000.dat", "var": ""}
+#        self.var_load("portfolio")
+
+m = Mintos(H)
+try:
+    print('Main part\n')
+#    m.varset["portfolio"]["var"] = json.loads(p.getPortfolio('0'))
+#    m.sync("portfolio")
+#    time.sleep(5)
+#    open('testfile')
+finally:
+    m.ulock()
+"""
+    // RETRIEVE LIST
+    file_put_contents('dat\port000000.dat', cwm::getPortfolio('0', $sig));
+    $ts = json_decode(file_get_contents('dat'.OS.'port000000.dat'));
+    // RETRIEVE TENDERS
+    foreach ($ts->ztenders as $t)
+	if ($t->State == 3 and !file_exists('dat'.OS.'port'.$t->CTenderID.'.dat')) 
+		file_put_contents('dat'.OS.'port'.$t->CTenderID.'.dat', cwm::getTender($t->CTenderID, $sig));
+		unset($t);
+		// RETRIEVE OPERATIONS
+		//!!! Copy numx3.dat, 
+		//	histx3op.dat, 
+		//	histx3ret.dat first !!!
+		//!!!	and back to Site after update !!!
+		//!!! Look to histX3ret.dat and observe all ret id's !!!
+		cwm::getX3hist(date('Ymd H:i:s',time()), $sig);
+		print "Checking ret's ID's...";
+		$ret = json_decode(file_get_contents(H.'dat'.OS.'histx3ret.dat'), true);
+		foreach ($ret as $idx => $v) 
+		    if(preg_match("/^[0-9]{10}$/", $v['org']) == 0)
+			    print "\nerror: org=".$v['org']; 
+			    unset($v);
+			    print "Done\n";
+			    exit(0);
+"""
