470119d0aec4cc2e5d8b83a1acb46d22ed4aa3c3
Maxim Alferov <malferov44@gmail.com>
2017-09-28 10:13:54 +0200

    rename main

--- mintos.py
+++ mintos.py
@@ -1,70 +1,70 @@
-#!/bin/env python3.6
 import sys
 import time
-import json
-from mi import MI
+import os
+import requests
+from bs4 import BeautifulSoup
+from sign.signdef import *
 from pdb import set_trace as bp
 
-H = './' # home dir
-
-class Runner(MI):
-    def __init__(self, h):
-        super().__init__()
-        self.h = h
-        self.data = {"status": {"file": "dat/numtdr.dat", "value": ""}}
-        self.data_load("status")
-        self.lock()
-    def data_sync(self, varname):
-        j = json.dumps(self.data[varname]["value"], sort_keys=True, indent=4)#separators=(',',':')
-        open(self.h + self.data[varname]["file"], 'w').write(j)
-    def data_load(self, varname):
-        self.data[varname]["value"] = json.loads(open(self.h + self.data[varname]["file"]).read())
-    def lock(self):
-        if self.data["status"]["value"][2] == 1:
-            self.ts_exit("System is locked")
-        else:
-            self.data["status"]["value"][2] = 1
-            self.data_sync("status")
-    def ulock(self):
-        self.data["status"]["value"][2] = 0
-        self.data_sync("status")
-
-class Mintos(Runner):
-    def __init__(self, h):
-        super().__init__(h)
-        self.numlist = {}
-
-m = Mintos(H)
-try:
-    print("Main part\n")
-    m.numlist = m.getNewLoans()
-    bp()
-#    open("out.txt", 'w').write(m.numlist)
-#    time.sleep(5)
-finally:
-    m.ulock()
+class MI:
+    def __init__(self):
+        self.host = "https://www.mintos.com/en"
+        self.user = os.environ['MINTOS_USER'] # export MINTOS_USER=mintosuser
+        self.passwd = os.environ['MINTOS_PASS'] # export MINTOS_PASS=secret
+    def ts_exit(self, msg):
+        sys.exit(time.strftime("%Y-%m-%d %H:%M:%S ") + str(msg))
+    def getNewLoans(self):
+        payload = {"_csrf_token": "4GCwzO2PT5tLH7hNChaO6S2dA0N4kq6ySb_CgHtyKTA", "_username": self.user, "_password": self.passwd}
+        session = requests.Session()
+        r = session.post(self.host + "/login/check", data = payload) # send auth form
+        if r.status_code != 200:
+            self.ts_exit("Unable to open login page")
+        r = session.get(self.host + "/available-loans/primary-market") # get loan list
+        if r.status_code != 200:
+            self.ts_exit("Unable to get loan list")
+        soup = BeautifulSoup(r.text, "html.parser") # response parsing
+        table = soup.find('table', {'id': 'primary-market-table'}) # Find users table
+        bp()
+        return {}
 """
-    // RETRIEVE LIST
-    file_put_contents('dat\port000000.dat', cwm::getPortfolio('0', $sig));
-    $ts = json_decode(file_get_contents('dat'.OS.'port000000.dat'));
-    // RETRIEVE TENDERS
-    foreach ($ts->ztenders as $t)
-	if ($t->State == 3 and !file_exists('dat'.OS.'port'.$t->CTenderID.'.dat')) 
-		file_put_contents('dat'.OS.'port'.$t->CTenderID.'.dat', cwm::getTender($t->CTenderID, $sig));
-		unset($t);
-		// RETRIEVE OPERATIONS
-		//!!! Copy numx3.dat, 
-		//	histx3op.dat, 
-		//	histx3ret.dat first !!!
-		//!!!	and back to Site after update !!!
-		//!!! Look to histX3ret.dat and observe all ret id's !!!
-		cwm::getX3hist(date('Ymd H:i:s',time()), $sig);
-		print "Checking ret's ID's...";
-		$ret = json_decode(file_get_contents(H.'dat'.OS.'histx3ret.dat'), true);
-		foreach ($ret as $idx => $v) 
-		    if(preg_match("/^[0-9]{10}$/", $v['org']) == 0)
-			    print "\nerror: org=".$v['org']; 
-			    unset($v);
-			    print "Done\n";
-			    exit(0);
+table_body = table.find('tbody') # Skip head, take body
+rows = table_body.find_all('tr') # All rows from table
+point = datetime.now() # Fix point of time
+recipients = []
+for row in rows:
+    cols = row.find_all('td') # All columns from iterating row
+    user = cols[2].get_text() # User email
+    recipients.append(user) # Add email to list of recipients
+    if datetime.strptime(cols[8].get_text(), "%Y-%m-%d %H:%M:%S") < point - timedelta(days=DELTA): # Break if expired
+        break
+
+
+
+"""
+"""
+    def sign(self, ss):
+        try:
+            return str(subprocess.check_output([self.wmsigner, "-s", ss]), 'ascii')
+        except subprocess.CalledProcessError as e:
+            self.ts_exit(e.output)
+    def getPortfolio(self, tid):
+        path = "/ZTenders.ashx"
+        t = str(int(time.time())) + '000'
+        ss = self.sign(mWMID + ';' + tid + ';' + t)
+        params = "WMID="+mWMID+"&TID="+tid+"&t="+t+"&VR=1&SS="+ss
+        return self.httpReq('https', path, params, 'get')
+#        return [t, str(self.sign(t))]
+    def httpReq(self, proto, path, params, reqtype):
+        session = requests.Session()
+        if reqtype == 'get':
+            r = session.get(proto + '://' + self.host + path + '?' + params)
+        elif reqtype == 'post':
+            payload = params
+            r = session.post(proto + '://' + self.host + path, data = payload)
+        else:
+            self.ts_exit("httpReq: illegal reqtype")
+        if r.status_code != 200:
+            self.ts_exit("httpReq: http_code = " + str(r.status_code))
+        else:
+            return r.text
 """
